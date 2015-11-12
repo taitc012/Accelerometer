98bcde48c816e74a84ee5b1aca0e66030c0310e3
taitc012 <taitc@cs.nctu.edu.tw>
2015-11-12 18:22:56 +0800

    output to file and add low pass filter with some bugs

--- app/src/main/java/com/example/taitc/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/taitc/accelerometer/MainActivity.java
@@ -9,6 +9,14 @@ package com.example.taitc.accelerometer;
         import android.os.Bundle;
         import android.os.Vibrator;
         import android.widget.TextView;
+        import java.io.BufferedWriter;
+        import java.io.File;
+        import java.io.FileWriter;
+        import java.io.FileOutputStream;
+        import java.io.IOException;
+        import java.util.Date;
+        import java.text.SimpleDateFormat;
+        import java.util.Calendar;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
@@ -25,9 +33,16 @@ public class MainActivity extends Activity implements SensorEventListener {
     private float deltaY = 0;
     private float deltaZ = 0;
 
+    private float mag = 0;
+
     private float vibrateThreshold = 0;
 
-    private TextView currentX, currentY, currentZ, maxX, maxY, maxZ;
+    float [] gravity={0,0,0};
+
+    FileOutputStream output;
+
+
+    private TextView currentX, currentY, currentZ, maxX, maxY, maxZ, magA;
 
     public Vibrator v;
 
@@ -51,6 +66,23 @@ public class MainActivity extends Activity implements SensorEventListener {
         //initialize vibration
         v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
 
+        try{
+            /*String data01 = "This is OutputStream Data01!";
+            String data02 = "\n";
+            String data03 = "Hello! This is Data02!!";
+            String data04 = "\n";*/
+            //建立FileOutputStream物件，路徑為SD卡中的output.txt
+            output = new FileOutputStream("/sdcard/output.txt");
+            /*output.write(data01.getBytes());  //write()寫入字串，並將字串以byte形式儲存。
+            output.write(data02.getBytes());   //利用getBytes()將字串內容換為Byte
+            output.write(data03.getBytes());
+            output.write(data04.getBytes());
+            output.close();*/
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 
     public void initializeViews() {
@@ -58,9 +90,11 @@ public class MainActivity extends Activity implements SensorEventListener {
         currentY = (TextView) findViewById(R.id.currentY);
         currentZ = (TextView) findViewById(R.id.currentZ);
 
-        maxX = (TextView) findViewById(R.id.maxX);
+        /*maxX = (TextView) findViewById(R.id.maxX);
         maxY = (TextView) findViewById(R.id.maxY);
-        maxZ = (TextView) findViewById(R.id.maxZ);
+        maxZ = (TextView) findViewById(R.id.maxZ);*/
+
+        magA = (TextView) findViewById(R.id.magA);
     }
 
     //onResume() register the accelerometer for listening the events
@@ -71,6 +105,11 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     //onPause() unregister the accelerometer for stop listening the events
     protected void onPause() {
+        try {
+            output.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         super.onPause();
         sensorManager.unregisterListener(this);
     }
@@ -88,27 +127,55 @@ public class MainActivity extends Activity implements SensorEventListener {
         // display the current x,y,z accelerometer values
         displayCurrentValues();
         // display the max x,y,z accelerometer values
-        displayMaxValues();
+        //displayMaxValues();
 
         // get the change of the x,y,z values of the accelerometer
-        deltaX = Math.abs(lastX - event.values[0]);
+        /*deltaX = Math.abs(lastX - event.values[0]);
         deltaY = Math.abs(lastY - event.values[1]);
-        deltaZ = Math.abs(lastZ - event.values[2]);
+        deltaZ = Math.abs(lastZ - event.values[2]);*/
 
-        // if the change is below 2, it is just plain noise
-        if (deltaX < 2)
-            deltaX = 0;
-        if (deltaY < 2)
-            deltaY = 0;
-        if ((deltaZ > vibrateThreshold) || (deltaY > vibrateThreshold) || (deltaZ > vibrateThreshold)) {
-            v.vibrate(50);
+        final float alpha = 0.1f;
+
+        gravity[0] = alpha * gravity[0] + (1-alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1-alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1-alpha) * event.values[2];
+
+        deltaX = event.values[0] - gravity[0];
+        deltaY = event.values[1] - gravity[1];
+        deltaZ = event.values[2] - gravity[2];
+
+        mag = (float) Math.sqrt( deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ );
+
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String format = s.format(new Date());
+        String newline = "\n";
+        String whitespace = " ";
+
+        try{
+
+            output.write(format.getBytes());
+            output.write(whitespace.getBytes());
+            output.write(Float.toString(mag).getBytes());
+            output.write(whitespace.getBytes());
+            output.write(Float.toString(deltaX).getBytes());
+            output.write(whitespace.getBytes());
+            output.write(Float.toString(deltaY).getBytes());
+            output.write(whitespace.getBytes());
+            output.write(Float.toString(deltaZ).getBytes());
+            output.write(newline.getBytes());
+        }catch(Exception e){
+            e.printStackTrace();
         }
+
+
+
     }
 
     public void displayCleanValues() {
         currentX.setText("0.0");
         currentY.setText("0.0");
         currentZ.setText("0.0");
+        magA.setText("0.0");
     }
 
     // display the current x,y,z accelerometer values
@@ -116,21 +183,8 @@ public class MainActivity extends Activity implements SensorEventListener {
         currentX.setText(Float.toString(deltaX));
         currentY.setText(Float.toString(deltaY));
         currentZ.setText(Float.toString(deltaZ));
+        magA.setText(Float.toString(mag));
     }
 
-    // display the max x,y,z accelerometer values
-    public void displayMaxValues() {
-        if (deltaX > deltaXMax) {
-            deltaXMax = deltaX;
-            maxX.setText(Float.toString(deltaXMax));
-        }
-        if (deltaY > deltaYMax) {
-            deltaYMax = deltaY;
-            maxY.setText(Float.toString(deltaYMax));
-        }
-        if (deltaZ > deltaZMax) {
-            deltaZMax = deltaZ;
-            maxZ.setText(Float.toString(deltaZMax));
-        }
-    }
+
 }
